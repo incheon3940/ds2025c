dd8f5a42d825922b4e95c063e8cf4cde42a220ad
incheon <incheon3940@gmail.com>
2025-03-24 14:26:19 +0900

    v0.4 array

--- week03.py
+++ week03.py
@@ -1,3 +1,9 @@
-l = [90, 8, -7, 0, 16]
-for i in range(len(l)):
-    print(f"{l[i]:2} {id([i])}")
+import array
+
+arr = array.array('f',[90, 8, -7, 0, 16])
+for i in range(len(arr)):
+    print(f"{arr[i]:2} {id(arr[i])}")
+
+l = [99, 99]
+print(id(0), id(1))
+

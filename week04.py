b9c3d59e43eb46ce6956290c18e581854e606a59
incheon <incheon3940@gmail.com>
2025-03-31 15:28:29 +0900

    v1.7 Linked list

--- week04.py
+++ week04.py
@@ -1,5 +1,3 @@
-import random
-
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -18,6 +16,17 @@ class LinkedList:
             current = current.link
         current.link = Node(data)
 
+    def remove(self, target):
+        if self.head.data == target:
+            self.head = self.head.link
+            return
+        current = self.head
+        previous = None
+        while current:
+            if current.data == target:
+                previous.link = current.link
+            previous = current
+            current = current.link
 
     def search(self, target):
         current = self.head
@@ -36,16 +45,13 @@ class LinkedList:
             current = current.link
         return out_texts + "END"
 
-# ll = LinkedList()
-# ll.append(8)
-# ll.append(10)
-# ll.append(-9)
-# print(ll)
-# print(ll.search(99))
-# print(ll.search(10))
-
 ll = LinkedList()
-for _ in range(0, 20):
-    ll.append(random.randint(1, 30))
+ll.append(8)
+ll.append(10)
+ll.append(-9)
 print(ll)
+print(ll.search(99))
 print(ll.search(10))
+ll.remove(10)
+ll.remove(8)
+print(ll)

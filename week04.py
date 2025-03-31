6bc5deb4b7e7691324920ee8426795727b0379ba
incheon <incheon3940@gmail.com>
2025-03-31 15:09:21 +0900

    v1.6 Linked list

--- week04.py
+++ week04.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -16,6 +18,7 @@ class LinkedList:
             current = current.link
         current.link = Node(data)
 
+
     def search(self, target):
         current = self.head
         while current.link:
@@ -33,10 +36,16 @@ class LinkedList:
             current = current.link
         return out_texts + "END"
 
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
+# print(ll)
+# print(ll.search(99))
+# print(ll.search(10))
+
 ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+for _ in range(0, 20):
+    ll.append(random.randint(1, 30))
 print(ll)
-print(ll.search(99))
 print(ll.search(10))

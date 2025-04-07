d64adb7f8c0f33c5237bd84b0cd47f0a6ed04214
incheon <incheon3940@gmail.com>
2025-04-07 14:50:14 +0900

    v2.1 stack

--- week05.py
+++ week05.py
@@ -1,9 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
+
 class Stack:
     def __init__(self):
         self.items = list()
+        self.top = None
 
     def push(self, item):
         self.items.append(item)
+
+    def push(self, data):
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.link = self.top
+            self.top = node  # top update
 
     def pop(self):
         return self.items.pop()
@@ -16,11 +31,17 @@ class Stack:
 
     def peek(self):
         return self.items[-1]
+        if self.top is None:
+            return "Stack is empty!"
+        popped_node = self.top
+        self.top = self.top.link
+        return popped_node.data
 
 
 s1 = Stack()
 s2 = Stack()
 print(s1.is_empty())
+print(s1.pop())
 s1.push("Data structure")
 print(s1.is_empty())
 print(s2.is_empty())
@@ -31,3 +52,4 @@ print(s1.size())
 print(s1.pop())
 print(s1.size())
 print(s1.peek())
+print(s1.pop())

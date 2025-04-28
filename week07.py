ca0f86827655d3223c21dd545ae9eac0382ebbc0
incheon <incheon3940@gmail.com>
2025-04-28 14:53:59 +0900

    v3.1 tree

--- week07.py
+++ week07.py
@@ -1,3 +1,24 @@
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end=' ')
+    pre_order(node.left)
+    pre_order(node.right)
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end=' ')
+    in_order(node.right)
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end='')
+
 class TreeNode:
     def __init__(self):
         self.left = None
@@ -27,5 +48,8 @@ node6 = TreeNode()
 node6.data = "sm"
 node3.left = node6
 
-print(node5.data)
-print(node1.left.right.data)
+post_order(node1)
+print()
+in_order(node1)
+print()
+pre_order(node1)

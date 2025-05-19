2a5e283e4085247300ed1a91ee565f834072e2ff
incheon <incheon3940@gmail.com>
2025-05-19 14:19:49 +0900

    v4.5 graph dfs

--- week10.py
+++ week10.py
@@ -72,11 +72,16 @@ def delete(node, value):
             return node.right
         elif node.right is None:
             return node.left
-        min_larger_node = node.right
-        while min_larger_node.left:
-            min_larger_node = min_larger_node.left
-        node.data = min_larger_node.data
-        node.right = delete(node.right, min_larger_node.data)
+        # min_larger_node = node.right
+        # while min_larger_node.left:
+        #     min_larger_node = min_larger_node.left
+        # node.data = min_larger_node.data
+        # node.right = delete(node.right, min_larger_node.data)
+        max_smaller_node = node.left
+        while max_smaller_node.right:
+            max_smaller_node = max_smaller_node.right
+        node.data = max_smaller_node.data
+        node.left = delete(node.left, max_smaller_node.data)
     return node
 
 if __name__ == "__main__":
